0cb4c751dcb1437be830ac775e621e281be07f96
Đức Nguyễn <nguyenduc1021996@gmail.com>
2023-06-08 11:23:52 +0700

    Add nav control

--- app/src/main/java/com/lkonlesoft/wally/MainActivity.kt
+++ app/src/main/java/com/lkonlesoft/wally/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.lkonlesoft.wally
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,16 +14,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.twotone.ArrowBack
-import androidx.compose.material.icons.twotone.Favorite
-import androidx.compose.material.icons.twotone.Home
-import androidx.compose.material.icons.twotone.Info
 import androidx.compose.material.icons.twotone.Search
-import androidx.compose.material.icons.twotone.Settings
-import androidx.compose.material.icons.twotone.Star
-import androidx.compose.material.icons.twotone.ThumbUp
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
@@ -41,21 +37,21 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
-import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.lkonlesoft.wally.ui.theme.WallyTheme
 import kotlinx.coroutines.CoroutineScope
@@ -72,39 +68,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainContext(onAboutClick = {startAboutActivity()}, onSettingClick = {startSettingActivity()})
+                    MainContext()
                 }
             }
         }
     }
-    private fun startSettingActivity(){
-        val intent = Intent(this, Setting::class.java)
-        startActivity(intent)
-    }
-    private fun startAboutActivity(){
-        val intent = Intent(this, About::class.java)
-        startActivity(intent)
-    }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
 
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainContext(onAboutClick: () -> Unit, onSettingClick: () -> Unit) {
+fun MainContext() {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-
+    val navController = rememberNavController()
     ModalNavigationDrawer(
         gesturesEnabled = true,
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
-                DrawerBar(drawerState = drawerState, scope = scope, onAboutClick = onAboutClick, onSettingClick = onSettingClick)
+                DrawerBar(drawerState = drawerState, scope = scope, navController = navController)
             }
         },
     ) {
@@ -127,25 +112,22 @@ fun MainContext(onAboutClick: () -> Unit, onSettingClick: () -> Unit) {
                     actions = {
                         IconButton(onClick = { } ) {
                             Icon(
-                                imageVector = Icons.TwoTone.Search, contentDescription = "Info"
+                                imageVector = Icons.TwoTone.Search, contentDescription = "Search"
                             )
                         }
                     },
                     modifier = Modifier.padding(4.dp)
                 )
             },
+            content = { padding ->
+                Box(modifier = Modifier.padding(padding)) {
+                    Navigation(navController = navController)
+                }
+            },
             bottomBar = {
-                NavBar()
+                NavBar(navController = navController)
             }
-        ) { paddingValues ->
-            Surface(
-                modifier = Modifier
-                    .padding(paddingValues)
-                    .fillMaxSize()
-            ) {
-                Greeting(name = "Android")
-            }
-        }
+        )
     }
 
 }
@@ -154,17 +136,18 @@ fun MainContext(onAboutClick: () -> Unit, onSettingClick: () -> Unit) {
 private fun DrawerMenuItem(
     imageVector: ImageVector,
     text: String,
+    route: String,
     onItemClick: () -> Unit){
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onItemClick() }
+            .clickable(onClick = onItemClick)
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically,
     ){
         Icon(
             imageVector = imageVector,
-            contentDescription = null,
+            contentDescription = route,
         )
         Spacer(modifier = Modifier.width(25.dp))
         Text(text = text)
@@ -173,7 +156,13 @@ private fun DrawerMenuItem(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DrawerBar(drawerState: DrawerState, scope: CoroutineScope, onAboutClick: () -> Unit, onSettingClick: () -> Unit){
+fun DrawerBar(drawerState: DrawerState, scope: CoroutineScope, navController: NavController){
+    val drawerList = listOf(
+        NavigationItem.Setting,
+        NavigationItem.RateUs,
+        NavigationItem.Donate,
+        NavigationItem.About
+    )
     Column(
         modifier = Modifier.fillMaxWidth()) {
         Row(
@@ -202,39 +191,209 @@ fun DrawerBar(drawerState: DrawerState, scope: CoroutineScope, onAboutClick: () 
             .fillMaxWidth())
         LazyColumn(modifier = Modifier.fillMaxWidth())
         {
-            item {DrawerMenuItem(imageVector = Icons.TwoTone.Settings, text = "Settings", onItemClick = onSettingClick)}
-            item {DrawerMenuItem(imageVector = Icons.TwoTone.Star, text = "Rate us", onItemClick = {})}
-            item {DrawerMenuItem(imageVector = Icons.TwoTone.ThumbUp, text = "Donate", onItemClick = {})}
-            item {DrawerMenuItem(imageVector = Icons.TwoTone.Info, text = "About", onItemClick = onAboutClick)}
+            items(drawerList) { item ->
+                DrawerMenuItem(
+                    imageVector = item.icon,
+                    text = item.tittle,
+                    route = item.route,
+                    onItemClick = {
+                        navController.navigate(item.route) {
+                            // Pop up to the start destination of the graph to
+                            // avoid building up a large stack of destinations
+                            // on the back stack as users select items
+                            navController.graph.startDestinationRoute?.let { route ->
+                                popUpTo(route) {
+                                    saveState = true
+                                }
+                            }
+                            // Avoid multiple copies of the same destination when
+                            // reselecting the same item
+                            launchSingleTop = true
+                            // Restore state when reselecting a previously selected item
+                            restoreState = true
+                        }
+                        scope.launch { drawerState.close() }
+                    }
+            )
+            }
         }
     }
 }
 
 
 @Composable
-fun NavBar(){
-    var selectedItem by rememberSaveable{ mutableStateOf(0)}
-
+fun NavBar(navController: NavController){
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.Favorite
+    )
     NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
             NavigationBarItem(
-                icon = { Icon(Icons.TwoTone.Home, contentDescription = "Home") },
-                label = { Text("Home") },
-                selected = selectedItem == 0,
-                onClick = { selectedItem = 0 }
+                icon = { Icon(imageVector = item.icon, contentDescription = item.route) },
+                label = { Text(item.tittle) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
             )
-            NavigationBarItem(
-                icon = { Icon(Icons.TwoTone.Favorite, contentDescription = "Favorites") },
-                label = { Text("Favorites") },
-                selected = selectedItem == 1,
-                onClick = { selectedItem = 1 }
-            )
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WallyTheme {
-        Greeting("Android")
+fun FavoriteScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+
+    ) {
+        Text(
+            text = "Favorites View",
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
     }
 }
+
+@Composable
+fun SettingScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+
+    ) {
+        Text(
+            text = "Settings View",
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Composable
+fun DonateScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+
+    ) {
+        Text(
+            text = "Donate View",
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Composable
+fun RateUsScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+
+    ) {
+        Text(
+            text = "Rate Us View",
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Composable
+fun AboutScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+
+    ) {
+        Text(
+            text = "About View",
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+
+    ) {
+        Text(
+            text = "Home View",
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController, startDestination = NavigationItem.Home.route) {
+        composable(NavigationItem.Home.route) {
+            HomeScreen()
+        }
+        composable(NavigationItem.Favorite.route) {
+            FavoriteScreen()
+        }
+        composable(NavigationItem.Setting.route) {
+            SettingScreen()
+        }
+        composable(NavigationItem.About.route) {
+            AboutScreen()
+        }
+        composable(NavigationItem.RateUs.route) {
+            RateUsScreen()
+        }
+        composable(NavigationItem.Donate.route) {
+            DonateScreen()
+        }
+    }
+}
+
